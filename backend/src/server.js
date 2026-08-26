16e4ad37145b1f1c056e24663b5d6b3f73395a61
Abhay <abhaypawar1304@gmail.com>
2026-08-26 08:03:27 +0530

    mongoose added in server file that was missing

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import app from "./app.js";
 import { config } from "./config/env.js";
 import { connectDB } from "./config/db.js";
+import mongoose from "mongoose";
 
 const startServer = async () => {
   try {
