39efbeee8ed185d353e0ef6ef00bcb63daf9b505
abhay <abhaypawar1304@gmail.com>
2026-09-17 11:07:37 +0530

    Update student.model.js

--- backend/src/models/student.model.js
+++ backend/src/models/student.model.js
@@ -75,7 +75,7 @@ const studentSchema = new mongoose.Schema(
   },
 );
 
-// Indexes for better query performance index optimization
+// Indexes for better query performance index optimization are done here
 // studentSchema.index({ email: 1 });
 // studentSchema.index({ studentId: 1 });
 studentSchema.index({ firstName: 1, lastName: 1 });
